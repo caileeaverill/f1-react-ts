283d9d4111609cb1add4ef8c6295b900b34a9e8b
Cailee Averill <caileeaverill@gmail.com>
2025-04-20 21:20:12 -0400

    Init repo

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+//pages
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
